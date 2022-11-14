74edf8b23ab70cac77abe55546d7d67219de215c
Leonel Matías Vazquez <leonel.m.vazquez@gmail.com>
2022-11-14 18:57:57 -0300

    Stress test with artillery

--- dist/controllers/IPGeolocationController.js
+++ dist/controllers/IPGeolocationController.js
@@ -10,7 +10,6 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_validator_1 = require("express-validator");
-const cache_1 = require("../cache");
 const services_1 = require("../services");
 const commons_1 = require("../commons");
 const ipTraces = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
@@ -21,9 +20,10 @@ const ipTraces = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
             return res.status(400).send(resultValidationReq);
         }
         const { ip } = req.body;
-        const result = yield services_1.IPGeolocationService.ipTraces(ip);
-        cache_1.appCache.set(ip, result === null || result === void 0 ? void 0 : result.data);
-        res.status(result === null || result === void 0 ? void 0 : result.status).send(result === null || result === void 0 ? void 0 : result.data);
+        console.log("ENTRA!");
+        //const result = await IPGeolocationService.ipTraces(ip);
+        //appCache.set(ip, result?.data);
+        res.status(200).send("OK");
     }
     catch (error) {
         commons_1.logger.error(error);
